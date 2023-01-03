a2293fb2cbf3d92aa1a9e020aa42b5c26914a7eb
Official-Dev-Vineet <viratsinghkaharwar8923@gmail.com>
2023-01-03 16:02:28 +0530

    ajax called

--- school/login.php
+++ school/login.php
@@ -1,38 +1,5 @@
-<?php
-require_once("private/db.inc.php");
-session_start();
-if (isset($_SESSION['username']) != "") {
-    header("Location: private/dashboard.php");
-}
-if (isset($_POST['login'])) {
-    $email = mysqli_real_escape_string($con, $_POST['email']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $email_error = "Please Enter Valid Email ID";
-    }
-    if (strlen($password) < 6) {
-        $password_error = "Password must be minimum of 6 characters";
-    }
-    $result = mysqli_query($con, "SELECT * FROM student_details WHERE email = '$email' and password = 'md5($password)'");
-    if (!empty($result)) {
-        if ($row = mysqli_fetch_array($result)) {
-            $_SESSION['user_id'] = $row['id'];
-            $_SESSION['user_name'] = $row['username'];
-            $_SESSION['user_email'] = $row['email'];
-            $_SESSION['user_image'] = $row['profileImage'];
-            header("location:private/dashboard.php");
-        }
-    } else {
-        $error_message = "Incorrect Email or Password!!!";
-        return "ok";
-    }
-    $con->close();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Login to your Dashboard </title>
@@ -92,7 +59,7 @@ if (isset($_POST['login'])) {
             $("#loginForm").submit(function () {
                 var loginData = new FormData($(this)[0]);
                 $.ajax({
-                    url: "login.php",
+                    url: "private/login_validate.php",
                     type: "POST",
                     data: loginData,
                     contentType: false,
--- school/private/login_validate.php
+++ school/private/login_validate.php
@@ -3,28 +3,32 @@ require_once("db.inc.php");
 session_start();
 if (isset($_SESSION['username']) != "") {
     header("Location: private/dashboard.php");
+    echo "all things are good you are redirecting to dahsboard";
 }
 if (isset($_POST['login'])) {
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $email_error = "Please Enter Valid Email ID";
+        echo "email not valid";
     }
     if (strlen($password) < 6) {
         $password_error = "Password must be minimum of 6 characters";
+        echo "password error";
     }
-    $result = mysqli_query($con, "SELECT * FROM student_details WHERE 'email' = $email and 'password' = md5($password)");
+    $result = mysqli_query($con, "SELECT * FROM student_details WHERE email = '$email' and password = 'md5($password)'");
     if (!empty($result)) {
         if ($row = mysqli_fetch_array($result)) {
             $_SESSION['user_id'] = $row['id'];
             $_SESSION['user_name'] = $row['username'];
             $_SESSION['user_email'] = $row['email'];
             $_SESSION['user_image'] = $row['profileImage'];
-            return "ok";
+            // header("location:private/dashboard.php");
+            echo "validate";
         }
     } else {
         $error_message = "Incorrect Email or Password!!!";
-        return "not ok";
+        echo "not validate";
     }
     $con->close();
 }
